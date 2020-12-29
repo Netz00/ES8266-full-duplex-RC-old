7e98cc4bf892a7537adcaee958103a6dbb109787
42-6f-17e-6f-20-44-75-72-64-6f-76 <bdurdo00@fesb.hr>
2020-12-29 15:29:50 +0100

    Added my project

---
+++ ARDUINO/CarDuino/CarDuino.ino
@@ -0,0 +1,138 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+
+const char* ssid = "1234";
+const char* password = "qwertz1234";
+
+#define bufferSize 255
+#define new_frequency 10  //use PWM frequency optimised for your hardware
+#define PWMN 5            //forward, D2
+#define PWMR 16           //backward, D1
+#define PWML 0            //left, D3
+#define PWMD 4            //right, D4
+#define localUdpPort 4210 //right, D4
+
+
+struct _pos;
+typedef struct _pos* pos;
+typedef struct _pos {
+  size_t forward;
+  size_t backward;
+  size_t left;
+  size_t right;  
+} CarPostion;
+
+
+WiFiUDP Udp;
+
+char* incomingPacket;  // buffer for incoming packets
+
+int forward = 0, forward2 = 0, direction1 = 0, direction2 = 0;
+int *pforward, *pforward2, *pdirection, *pdirection2;
+
+int packetSize;
+int len;
+pos data;
+
+void setup()
+{
+  delay(1000);
+
+  Serial.begin(115200);
+
+  incomingPacket = NULL;
+  data = NULL;
+
+  incomingPacket = (char*)malloc(bufferSize * sizeof(char));
+  data = (pos)malloc(sizeof(CarPostion));
+
+  if (incomingPacket == NULL)
+    Serial.printf("Failed to allocate memory.\n");
+  if (data == NULL)
+    Serial.printf("Failed to allocate memory.\n");
+
+  analogWriteFreq(new_frequency);
+  pinMode(PWMN, OUTPUT);
+  pinMode(PWMR, OUTPUT);
+  pinMode(PWML, OUTPUT);
+  pinMode(PWMD, OUTPUT);
+
+  analogWrite(PWMN, 0);
+  analogWrite(PWMR, 0);
+  analogWrite(PWML, 0);
+  analogWrite(PWMD, 0);
+
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+  Udp.begin(localUdpPort);
+  Udp.flush();
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
+
+  
+}
+
+
+void loop()
+{
+  packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    len = Udp.read(incomingPacket, bufferSize);
+    if (len > 0)
+    {
+      incomingPacket[len] = 0;
+    }
+    //Serial.printf("Packet size: %d\nPacket: %s\n",len,incomingPacket);
+    sscanf(incomingPacket, "%u&%u:%u&%ue", &(data->forward), &(data->backward), &(data->left), &(data->right));
+    changeOfDirection();
+  }
+
+  delay (10);
+}
+
+inline void changeOfDirection()
+{
+  if (data->forward != data->backward)
+  {
+    if (data->backward == 0) {
+      analogWrite(PWMR, 0);
+      analogWrite(PWMN, data->forward);
+    }
+    if (data->forward == 0) {
+      analogWrite(PWMN, 0);
+      analogWrite(PWMR, data->backward);
+    }
+  }
+  else
+  {
+    analogWrite(PWMN, 0);
+    analogWrite(PWMR, 0);
+  }
+
+  if (data->left != data->right)
+  {
+    if (data->right == 0) {
+      analogWrite(PWMD, 0);
+      analogWrite(PWML, data->left);
+    }
+    if (data->left == 0) {
+      analogWrite(PWML, 0);
+      analogWrite(PWMD, data->right);
+    }
+  }
+
+  else
+  {
+    analogWrite(PWML, 0);
+    analogWrite(PWMD, 0);
+  }
+
+}
---
+++ AUTIC_WIFI_APP/RCar/app/src/main/java/com/example/rcar/MessageSender.java
@@ -0,0 +1,52 @@
+package com.example.rcar;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class MessageSender extends AsyncTask<String, Void, Void> {
+
+
+    public static InetAddress ip;
+    public static int port;
+    int msg_length;
+    byte[] message;
+    DatagramSocket s;
+    DatagramPacket p;
+    String udpStr;
+
+
+
+    @Override
+    protected Void doInBackground(String... voids) {
+
+        if(this.ip!=null) {
+            this.udpStr = voids[0];
+
+
+            this.msg_length = udpStr.length();
+            this.message = udpStr.getBytes();
+
+            DatagramSocket s = null;
+            try {
+                s = new DatagramSocket();
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+
+            this.p = new DatagramPacket(message, msg_length, this.ip, this.port);
+            try {
+                s.send(p);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+}
